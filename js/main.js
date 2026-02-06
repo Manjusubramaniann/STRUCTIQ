0f00e78595c1c48cd4e5657f754aec33bbd058a3
AYYAKKON G <iyyakone4372@gmail.com>
2026-02-07 02:38:15 +0530

    pdf issue final using github 2

--- js/main.js
+++ js/main.js
@@ -88,14 +88,9 @@ document.addEventListener("click", () => {
 });
 
 /* Prevent nav clicks from closing */
-// nav.addEventListener("click", (e) => {
-//   e.stopPropagation();
-// });
-if(nav){
-  nav.addEventListener("click", (e) => {
-    e.stopPropagation();
-  });
-}
+nav.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
 
 /* Close menu on nav link click */
 document.querySelectorAll(".nav a").forEach(link => {
@@ -476,32 +471,6 @@ function openPDF(file){
 // }
 
 
-// function renderPDF(){
-
-//     const canvas = document.getElementById("pdfCanvas");
-//     if(!canvas) return;
-
-//     const ctx = canvas.getContext("2d");
-
-//     pdfjsLib.getDocument(currentPDF).promise.then(function(pdf){
-
-//         pdfDoc = pdf;
-
-//         pdfDoc.getPage(pageNum).then(function(page){
-
-//             const viewport = page.getViewport({ scale: zoomLevel });
-
-//             canvas.height = viewport.height;
-//             canvas.width = viewport.width;
-
-//             page.render({
-//                 canvasContext: ctx,
-//                 viewport: viewport
-//             });
-//         });
-//     });
-// }
-
 function renderPDF(){
 
     const canvas = document.getElementById("pdfCanvas");
@@ -515,23 +484,19 @@ function renderPDF(){
 
         pdfDoc.getPage(pageNum).then(function(page){
 
-            const containerWidth = document.getElementById("pdfModal").clientWidth;
+            const viewport = page.getViewport({ scale: zoomLevel });
 
-            const viewport = page.getViewport({ scale: 1 });
-            const scaleRequired = containerWidth / viewport.width;
-
-            const scaledViewport = page.getViewport({ scale: scaleRequired });
-
-            canvas.height = scaledViewport.height;
-            canvas.width = scaledViewport.width;
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
 
             page.render({
                 canvasContext: ctx,
-                viewport: scaledViewport
+                viewport: viewport
             });
         });
     });
 }
+
 
 
 function zoomIn(){
